cd9c26143bf00feb1c8c59e14955676f96c4e848
Iwasa Kazmi <kzmizzz@gmail.com>
2021-02-14 17:17:21 +0900

    Add unsafe block

--- src/wpd/device.rs
+++ src/wpd/device.rs
@@ -27,18 +27,26 @@ impl Device {
     pub fn open(info: &DeviceInfo) -> Result<Device, Error> {
         let device: IPortableDevice = co_create_instance(&PortableDevice)?;
         let values: IPortableDeviceValues = co_create_instance(&PortableDeviceValues)?;
-        device.Open(info.id.as_ptr(), Some(values)).ok()?;
+        unsafe {
+            device.Open(info.id.as_ptr(), Some(values)).ok()?;
+        }
 
         let mut content_receptor: Option<IPortableDeviceContent> = None;
-        device.Content(&mut content_receptor).ok()?;
+        unsafe {
+            device.Content(&mut content_receptor).ok()?;
+        }
         let content = content_receptor.unwrap();
 
         let mut properties_receptor: Option<IPortableDeviceProperties> = None;
-        content.Properties(&mut properties_receptor).ok()?;
+        unsafe {
+            content.Properties(&mut properties_receptor).ok()?;
+        }
         let properties = properties_receptor.unwrap();
 
         let mut resources_receptor: Option<IPortableDeviceResources> = None;
-        content.Transfer(&mut resources_receptor).ok()?;
+        unsafe {
+            content.Transfer(&mut resources_receptor).ok()?;
+        }
         let resources = resources_receptor.unwrap();
 
         Ok(Device {
@@ -53,18 +61,16 @@ impl Device {
         ContentObject { id: vec![0u16] } // empty string
     }
 
-    pub fn get_objects<F>(
-        &self,
-        parent: &ContentObject,
-        mut callback: F,
-    ) -> Result<(), Error>
+    pub fn get_objects<F>(&self, parent: &ContentObject, mut callback: F) -> Result<(), Error>
     where
         F: FnMut(&ContentObject) -> Result<(), Error>,
     {
         let mut enum_object_ids_receptor: Option<IEnumPortableDeviceObjectIDs> = None;
-        self.content
-            .EnumObjects(0, parent.id.as_ptr(), None, &mut enum_object_ids_receptor)
-            .ok()?;
+        unsafe {
+            self.content
+                .EnumObjects(0, parent.id.as_ptr(), None, &mut enum_object_ids_receptor)
+                .ok()?;
+        }
         let enum_object_ids = enum_object_ids_receptor.unwrap();
 
         const ARRAY_SIZE: u32 = 32;
@@ -73,7 +79,10 @@ impl Device {
             // because the obtained strings must be freed with its destructor.
             let mut object_ids = WStrPtrArray::create(ARRAY_SIZE);
             let mut read = 0u32;
-            let err = enum_object_ids.Next(object_ids.size(), object_ids.as_mut_ptr(), &mut read);
+            let err;
+            unsafe {
+                err = enum_object_ids.Next(object_ids.size(), object_ids.as_mut_ptr(), &mut read);
+            }
             err.ok()?;
 
             for id in object_ids.to_vec(read) {
@@ -90,41 +99,56 @@ impl Device {
     pub fn get_object_name(&self, object: &ContentObject) -> Result<String, Error> {
         let key_collection: IPortableDeviceKeyCollection =
             co_create_instance(&PortableDeviceKeyCollection)?;
-        key_collection.Add(&WPD_OBJECT_NAME).ok()?;
-        key_collection.Add(&WPD_OBJECT_CONTENT_TYPE).ok()?;
-        key_collection.Add(&WPD_FUNCTIONAL_OBJECT_CATEGORY).ok()?;
+        unsafe {
+            key_collection.Add(&WPD_OBJECT_NAME).ok()?;
+            key_collection.Add(&WPD_OBJECT_CONTENT_TYPE).ok()?;
+            key_collection.Add(&WPD_FUNCTIONAL_OBJECT_CATEGORY).ok()?;
+        }
 
         let mut values_receptor: Option<IPortableDeviceValues> = None;
-        self.properties
-            .GetValues(
-                object.id.as_ptr(),
-                Some(key_collection),
-                &mut values_receptor,
-            )
-            .ok()?;
+        unsafe {
+            self.properties
+                .GetValues(
+                    object.id.as_ptr(),
+                    Some(key_collection),
+                    &mut values_receptor,
+                )
+                .ok()?;
+        }
         let values = values_receptor.unwrap();
 
-        let mut object_name = WStrPtr::create();
-        values
-            .GetStringValue(&WPD_OBJECT_NAME, object_name.as_mut_ptr())
-            .ok()?;
-
-        let mut content_type_clsid = Guid::zeroed();
-        values
-            .GetGuidValue(&WPD_OBJECT_CONTENT_TYPE, &mut content_type_clsid)
-            .ok()?;
-
-        let mut category_clsid = Guid::zeroed();
-        if content_type_clsid == WPD_CONTENT_TYPE_FUNCTIONAL_OBJECT {
+        let mut object_name_ptr = WStrPtr::create();
+        unsafe {
             values
-                .GetGuidValue(&WPD_FUNCTIONAL_OBJECT_CATEGORY, &mut category_clsid)
+                .GetStringValue(&WPD_OBJECT_NAME, object_name_ptr.as_mut_ptr())
+                .ok()?;
+        }
+        let object_name = object_name_ptr.to_string();
+
+        let mut content_type = Guid::zeroed();
+        unsafe {
+            values
+                .GetGuidValue(&WPD_OBJECT_CONTENT_TYPE, &mut content_type)
                 .ok()?;
         }
 
-        println!("   {:?}", content_type_clsid);
-        println!("   {:?}", category_clsid);
+        let mut functional_object_category = Guid::zeroed();
 
-        Ok(object_name.to_string())
+        if content_type == WPD_CONTENT_TYPE_FUNCTIONAL_OBJECT {
+            unsafe {
+                values
+                    .GetGuidValue(
+                        &WPD_FUNCTIONAL_OBJECT_CATEGORY,
+                        &mut functional_object_category,
+                    )
+                    .ok()?;
+            }
+        }
+
+        println!("   {:?}", content_type);
+        println!("   {:?}", functional_object_category);
+
+        Ok(object_name)
     }
 }
 
--- src/wpd/manager.rs
+++ src/wpd/manager.rs
@@ -29,23 +29,27 @@ impl Manager {
         for mut device_id in device_ids {
             // get name length
             let mut name_buf_len = 0u32;
-            self.manager
-                .GetDeviceFriendlyName(
-                    device_id.as_mut_ptr(),
-                    std::ptr::null_mut(),
-                    &mut name_buf_len as *mut u32,
-                )
-                .ok()?;
+            unsafe {
+                self.manager
+                    .GetDeviceFriendlyName(
+                        device_id.as_mut_ptr(),
+                        std::ptr::null_mut(),
+                        &mut name_buf_len as *mut u32,
+                    )
+                    .ok()?;
+            }
 
             // get name
             let mut name_buf = WStrBuf::create(name_buf_len);
-            self.manager
-                .GetDeviceFriendlyName(
-                    device_id.as_mut_ptr(),
-                    name_buf.as_mut_ptr(),
-                    &mut name_buf_len as *mut u32,
-                )
-                .ok()?;
+            unsafe {
+                self.manager
+                    .GetDeviceFriendlyName(
+                        device_id.as_mut_ptr(),
+                        name_buf.as_mut_ptr(),
+                        &mut name_buf_len as *mut u32,
+                    )
+                    .ok()?;
+            }
 
             let name = name_buf.to_string(name_buf_len - 1); // exclude null terminator
 
@@ -61,15 +65,19 @@ impl Manager {
     fn get_device_ids(&self) -> Result<Vec<IDStr>, Error> {
         // get number of devices
         let mut device_id_count = 0u32;
-        self.manager
-            .GetDevices(std::ptr::null_mut(), &mut device_id_count)
-            .ok()?;
+        unsafe {
+            self.manager
+                .GetDevices(std::ptr::null_mut(), &mut device_id_count)
+                .ok()?;
+        }
 
         // get device ids
         let mut device_ids = WStrPtrArray::create(device_id_count);
-        self.manager
-            .GetDevices(device_ids.as_mut_ptr(), &mut device_id_count)
-            .ok()?;
+        unsafe {
+            self.manager
+                .GetDevices(device_ids.as_mut_ptr(), &mut device_id_count)
+                .ok()?;
+        }
 
         Ok(device_ids.to_vec_all())
     }
