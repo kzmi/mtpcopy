ce654da4ab6382bf6b7c1706a38dddc67c8af054
Iwasa Kazmi <kzmizzz@gmail.com>
2021-06-21 01:37:38 +0900

    improve timestamp comparison

--- src/copy/copy_processor.rs
+++ src/copy/copy_processor.rs
@@ -3,13 +3,30 @@ use chrono::NaiveDateTime;
 use super::{destination_folder::DestinationFolder, file_info::FileInfo};
 
 pub trait CopyProcessor {
-    fn copy_as(&self, name: &str, dest: &mut impl DestinationFolder, recursive: bool) -> Result<(), Box<dyn std::error::Error>>;
+    fn copy_as(
+        &self,
+        name: &str,
+        dest: &mut impl DestinationFolder,
+        recursive: bool,
+    ) -> Result<(), Box<dyn std::error::Error>>;
 }
 
 pub fn can_skip_copying(src_file_info: &FileInfo, dest_file_info: &FileInfo) -> bool {
-    if let Some(src_time) = get_file_time(src_file_info) {
-        if let Some(dest_time) = get_file_time(dest_file_info) {
-            return src_time <= dest_time;
+    if src_file_info.data_size == dest_file_info.data_size {
+        if let Some(src_time) = get_file_time(src_file_info) {
+            if let Some(dest_time) = get_file_time(dest_file_info) {
+                // use .timestamp() value to ignore the subsecond
+                let src_ts = src_time.timestamp();
+                let dest_ts = dest_time.timestamp();
+                log::debug!(
+                    "src_time = {:?} ({:?})  dest_time = {:?} ({:?})",
+                    &src_time,
+                    &src_ts,
+                    &dest_time,
+                    &dest_ts
+                );
+                return src_ts <= dest_ts;
+            }
         }
     }
     false
